886dd2e458c20904d7fca01a2bac94778ec1b600
iashchuk <221box@list.ru>
2018-05-05 18:41:59 +0300

    Работоспособность мобильного меню без JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,35 @@
-function openMenu() {
-  var element = document.getElementById("mobile-menu");
-  /* element.classList.remove("page-header__js");*/
-  element.classList.toggle("page-header__nav--open");
+// function openMenu() {
+//   var element = document.getElementById("mobile-menu");
+//   element.classList.toggle("page-header__nav--open");
+// }
+var menuBurger = document.querySelector(".page-header__toggle");
+var openMenu = document.querySelector(".page-header__nav");
+var closeMenu = document.querySelector(".burger-button");
 
+
+if (openMenu.classList.contains("page-header__nav--open")) {
+  openMenu.classList.remove("page-header__nav--open");
 }
 
-/*
-function closeMenu() {
-  var iconBurger = document.getElementById("burger");
-  iconBurger.classList.remove("burger-button");
-  iconBurger.classList.add("burger-button--close");
+if (closeMenu.classList.contains("burger-button--close")) {
+  closeMenu.classList.remove("burger-button--close");
 }
-*/
+
+menuBurger.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openMenu.classList.toggle("page-header__nav--open");
+  closeMenu.classList.toggle("burger-button--close");
+});
+
+// Закрытие меню по клаваише ESC
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (openMenu.classList.contains("page-header__nav--open")) {
+      openMenu.classList.remove("page-header__nav--open");
+    }
+    if (closeMenu.classList.contains("burger-button--close")) {
+      closeMenu.classList.remove("burger-button--close");
+    }
+  }
+});
