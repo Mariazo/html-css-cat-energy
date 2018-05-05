5aae59be8eee0e0f842bcee489ddf6cf4fc575f6
iashchuk <221box@list.ru>
2018-05-05 04:57:44 +0300

    Минификация изображений. WebP

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,7 @@ gulp.task("images", function () {
         imagemin.jpegtran({progressive: true}),
         imagemin.svgo()
     ]))
-      .pipe(gulp.dest("source/img"));
+      .pipe(gulp.dest("source/img/optim"));
 });
 
 
