f799e2b40855456850f483ae3ec4367a3bfbf5b2
iashchuk <221box@list.ru>
2018-05-06 14:36:10 +0300

    Чистка кода

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,3 @@
-// function openMenu() {
-//   var element = document.getElementById("mobile-menu");
-//   element.classList.toggle("page-header__nav--open");
-// }
 var menuBurger = document.querySelector(".page-header__toggle");
 var openMenu = document.querySelector(".page-header__nav");
 var closeMenu = document.querySelector(".page-header__toggle");
